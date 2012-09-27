1971f426bbe040b832e621efb287ce67f146bf5d
domanu <dawid.de@hotmail.com>
2012-09-27 15:32:38 +0200

    Added js simple code

---
+++ jaxygen-core/src/main/java/org/jaxygen/netservice/html/HTMLPre.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2012 domanu.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jaxygen.netservice.html;
+
+/**
+ *
+ * @author domanu
+ */
+public class HTMLPre extends BasicHTMLElement implements HTMLElement {
+
+    String caption;
+
+    public HTMLPre() {
+        super("PRE");
+    }
+
+    public HTMLPre(final String text) {
+        super("PRE", text);
+        caption = text;
+    }
+
+    public HTMLPre(final String id, final String text) {
+        super("PRE", id);
+        caption = text;
+    }
+
+    @Override
+    public String renderContent() {
+        if (caption != null) {
+            caption = caption.replace("<", "&lt;");
+            caption = caption.replace(">", "&gt;");
+            caption = caption.replace(" ", "&nbsp;");
+        } else {
+            caption = "";
+        }
+        return caption;
+    }
+}
