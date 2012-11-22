7644f212205b99e3055544671639656e104f9fa3
vicctor <artur.keska@gmail.com>
2012-11-22 21:56:47 +0100

    added HTMLFrame class pom updated to the lates version of the apache FileUpload

---
+++ jaxygen-core/src/main/java/org/jaxygen/netservice/html/HTMLIFrame.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jaxygen.netservice.html;
+
+/**
+ *
+ * @author Artur Keska
+ */
+public class HTMLIFrame extends BasicHTMLElement {
+public final static String TAG = "iframe";
+ public HTMLIFrame() {
+  super(TAG);
+ }
+
+ public HTMLIFrame(final String id) {
+  super(TAG, id);
+ }
+}
