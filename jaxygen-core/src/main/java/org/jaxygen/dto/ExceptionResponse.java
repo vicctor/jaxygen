0de34b7c494df340ac446db079312236faef1738
vicctor <artur.keska@gmail.com>
2012-11-28 22:15:55 +0100

    Added stacktrace to the exception response

--- jaxygen-core/src/main/java/org/jaxygen/dto/ExceptionResponse.java
+++ jaxygen-core/src/main/java/org/jaxygen/dto/ExceptionResponse.java
@@ -4,56 +4,72 @@
  */
 package org.jaxygen.dto;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.nio.charset.Charset;
+
 /**
  *
  * @author Artur Keska
  */
 public class ExceptionResponse extends Response {
 
-    public static class ExceptionData {
+ public static class ExceptionData {
 
-        private String name;
-        private String description;
-        private String exceptionClass = "UnknownException";
+  private String name;
+  private String description;
+  private String exceptionClass = "UnknownException";
+  private String stackTrace;
 
-        public String getDescription() {
-            return description;
-        }
+  public String getDescription() {
+   return description;
+  }
 
-        public void setDescription(String description) {
-            this.description = description;
-        }
+  public void setDescription(String description) {
+   this.description = description;
+  }
 
-        public String getName() {
-            return name;
-        }
+  public String getName() {
+   return name;
+  }
 
-        public void setName(String name) {
-            this.name = name;
-        }
+  public void setName(String name) {
+   this.name = name;
+  }
 
-        public String getExceptionClass() {
-            return exceptionClass;
-        }
+  public String getExceptionClass() {
+   return exceptionClass;
+  }
 
-        public void setExceptionClass(String exceptionClass) {
-            this.exceptionClass = exceptionClass;
-        }
-    }
-    
-    ExceptionData exception = new ExceptionData();
+  public void setExceptionClass(String exceptionClass) {
+   this.exceptionClass = exceptionClass;
+  }
 
-    public ExceptionResponse() {
-    }
+  public String getStackTrace() {
+   return stackTrace;
+  }
 
-    public ExceptionResponse(String name, String description) {        
-        exception.setName(name);
-        exception.setDescription(description);        
-    }
+  public void setStackTrace(String stackTrace) {
+   this.stackTrace = stackTrace;
+  }
+ }
+ ExceptionData exception = new ExceptionData();
 
-    public ExceptionResponse(Throwable ex, final String message) {        
-        exception.setName(ex.toString());
-        exception.setDescription(message);
-        exception.setExceptionClass(ex.getClass().getCanonicalName());
-    }
+ public ExceptionResponse() {
+ }
+
+ public ExceptionResponse(String name, String description) {
+  exception.setName(name);
+  exception.setDescription(description);
+ }
+
+ public ExceptionResponse(Throwable ex, final String message) {
+  exception.setName(ex.toString());
+  exception.setDescription(message);
+  exception.setExceptionClass(ex.getClass().getCanonicalName());
+  ByteArrayOutputStream bo = new ByteArrayOutputStream();  
+  ex.printStackTrace(new PrintWriter(bo));
+  exception.setStackTrace(new String(bo.toByteArray(),Charset.defaultCharset()));
+ }
 }
