60fe015a73e89c049e5e983c1631914c784d77d8
imfact02 <imfact02@imfact02-PC.home>
2013-03-03 08:58:13 +0100

    Added generic collection container/base

---
+++ jaxygen-core/src/main/java/org/jaxygen/dto/collections/PaginableCollectionResponseBaseDTO.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2013 imfact02.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jaxygen.dto.collections;
+
+import java.util.Collection;
+
+/**This is a base class for paginable collection responses
+ * When building an response one has to create a derived class
+ * of this having concrete generic type of managed collection.
+ *
+ * @author ak
+ */
+public class PaginableCollectionResponseBaseDTO<T extends Collection> {
+    private T elements;
+    private long size;
+
+    public T getElements() {
+        return elements;
+    }
+
+    public void setElements(T elements) {
+        this.elements = elements;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+}
