9055799a2d0db58b92a1066c4a0d92db988dc984
Artur Keska <artur.keska@gmail.com>
2014-11-03 09:27:34 +0100

    Enable for nested converters.
    Since now converters that are able to convert from upper classes are used in case if there's no strict converter provided.
    Assuming that class A extends B
    
    Converter<B,C>
    
    Will be used when convert(a, c).
    But if registered converters
    Converter<A,C>
    Converter<B,C>
    Then in case convert(a,c) will be used Converter<A,C>

--- jaxygen-typeconverter/src/main/java/org/jaxygen/typeconverter/TypeConverterFactory.java
+++ jaxygen-typeconverter/src/main/java/org/jaxygen/typeconverter/TypeConverterFactory.java
@@ -19,71 +19,91 @@ import java.util.HashMap;
 import java.util.Map;
 import org.jaxygen.typeconverter.exceptions.ConversionError;
 
-/**Class is a registry of the TypeConverter classes. Once the converter registered
- * it will be used whenever once calls {@link TypeConverterFactory#convert(java.lang.Object, java.lang.Class)} method.
- * Factory automatically selects the required converter object by convert method 
- * parameter classes.
- * 
+/**
+ * Class is a registry of the TypeConverter classes. Once the converter
+ * registered it will be used whenever once calls
+ * {@link TypeConverterFactory#convert(java.lang.Object, java.lang.Class)}
+ * method. Factory automatically selects the required converter object by
+ * convert method parameter classes.
+ *
  *
  * @author Artur
  */
 public class TypeConverterFactory {
+
     public final static String DEFAULT_FACTORY = ".default_TypeConverterFactory";
-  private final Map<Class, Map<Class, TypeConverter>> converters = new HashMap<Class, Map<Class, TypeConverter>>();
-  private final static Map<String, TypeConverterFactory> factories = new HashMap<String, TypeConverterFactory>();
-  
-  static {
-      factories.put(DEFAULT_FACTORY, new TypeConverterFactory());
-  }
-  
-  /** Get default instance of the TypeConverterFactory class.
-   * 
-   * @return 
-   */
-  public static TypeConverterFactory instance() {
-     return factories.get(DEFAULT_FACTORY);
-  }
-  
-  /** Get named instance of the TypeConverterFactory class.
-   * 
+    private final Map<Class, Map<Class, TypeConverter>> converters = new HashMap<Class, Map<Class, TypeConverter>>();
+    private final static Map<String, TypeConverterFactory> factories = new HashMap<String, TypeConverterFactory>();
+
+    static {
+        factories.put(DEFAULT_FACTORY, new TypeConverterFactory());
+    }
+
+    /**
+     * Get default instance of the TypeConverterFactory class.
+     *
+     * @return
+     */
+    public static TypeConverterFactory instance() {
+        return factories.get(DEFAULT_FACTORY);
+    }
+
+    /**
+     * Get named instance of the TypeConverterFactory class.
+     *
      * @param name specific name of the factory.
-   * @return 
-   */
-  public static TypeConverterFactory instance(final String name) {
-     return factories.get(name);
-  }
-  
-  /**Add a new converter to this converters factory.
-   * 
-   * @param converter 
-   */
-  public void registerConverter(TypeConverter converter) {
+     * @return
+     */
+    public static TypeConverterFactory instance(final String name) {
+        return factories.get(name);
+    }
+
+    /**
+     * Add a new converter to this converters factory.
+     *
+     * @param converter
+     */
+    public void registerConverter(TypeConverter converter) {
         final Class from = converter.from();
         Map<Class, TypeConverter> toMap;
-      if (converters.containsKey(from) == false) {
-          toMap = new HashMap<Class, TypeConverter>();
-          converters.put(from, toMap);
-      } else {
-          toMap = converters.get(from);
-      }
-      toMap.put(converter.to(), converter);
-  }
-  
-  /**Get the converter which could translate an object from class from to class to.
-   * 
-   * @param <FROM>
-   * @param <TO>
-   * @param from
-   * @param to
-   * @return 
-   */
-  public <FROM, TO> TypeConverter<FROM, TO> get(Class<FROM> from, Class<TO> to) {
-      TypeConverter<FROM, TO> converter = null;
-      if (converters.containsKey(from) && converters.get(from).containsKey(to)) {
-          converter = converters.get(from).get(to);
-      }
-    return converter;
-  };
+        if (converters.containsKey(from) == false) {
+            toMap = new HashMap<Class, TypeConverter>();
+            converters.put(from, toMap);
+        } else {
+            toMap = converters.get(from);
+        }
+        toMap.put(converter.to(), converter);
+    }
+
+    /**
+     * Get the converter which could translate an object from class from to
+     * class to.
+     *
+     * @param <FROM>
+     * @param <TO>
+     * @param from
+     * @param to
+     * @return
+     */
+    public <FROM, TO> TypeConverter<FROM, TO> get(Class<FROM> from, Class<TO> to) {
+        TypeConverter<FROM, TO> converter = null;
+        if (converters.containsKey(from) && converters.get(from).containsKey(to)) {
+            converter = converters.get(from).get(to);
+        }
+        if (converter == null) {
+            for (Map<Class, TypeConverter> tcm : converters.values()) {
+                for (TypeConverter tc : tcm.values()) {
+                    if (tc.from().isAssignableFrom(from) && to.isAssignableFrom(tc.to())) {
+                        converter = tc;
+                        break;
+                    }
+                }
+            }
+        }
+        return converter;
+    }
+
+    ;
   
   /** Find converter that converts given object from class FROM to class TO.
    * Note that the from parameter could not be null. If you expect that from 
@@ -97,30 +117,31 @@ public class TypeConverterFactory {
    * @throws ConversionError 
    */
   public <FROM, TO> TO convert(FROM from, Class<TO> toClass) throws ConversionError {
-      @SuppressWarnings("unchecked")
-      TypeConverter<FROM, TO> converter = get((Class<FROM>)from.getClass(), toClass);
-      if (converter == null) {
-          throw new ConversionError("Could not find converter from class " + from.getClass() + " to class " + toClass);
-      }
-      return (TO) converter.convert(from);
-  }
-  
-  /** Convenient method used in case if the from object could be null
-   * 
-   * @param <FROM>
-   * @param <TO>
-   * @param from
-   * @param fromClass
-   * @param toClass
-   * @return
-   * @throws ConversionError 
-   */
-  public <FROM, TO> TO convert(FROM from, Class<FROM> fromClass, Class<TO> toClass) throws ConversionError {
-      @SuppressWarnings("unchecked")
-      TypeConverter<FROM, TO> converter = get(fromClass, toClass);
-      if (converter == null) {
-          throw new ConversionError("Could not find converter from class " + fromClass + " to class " + toClass);
-      }
-      return (TO) converter.convert(from);
-  }
+        @SuppressWarnings("unchecked")
+        TypeConverter<FROM, TO> converter = get((Class<FROM>) from.getClass(), toClass);
+        if (converter == null) {
+            throw new ConversionError("Could not find converter from class " + from.getClass() + " to class " + toClass);
+        }
+        return (TO) converter.convert(from);
+    }
+
+    /**
+     * Convenient method used in case if the from object could be null
+     *
+     * @param <FROM>
+     * @param <TO>
+     * @param from
+     * @param fromClass
+     * @param toClass
+     * @return
+     * @throws ConversionError
+     */
+    public <FROM, TO> TO convert(FROM from, Class<FROM> fromClass, Class<TO> toClass) throws ConversionError {
+        @SuppressWarnings("unchecked")
+        TypeConverter<FROM, TO> converter = get(fromClass, toClass);
+        if (converter == null) {
+            throw new ConversionError("Could not find converter from class " + fromClass + " to class " + toClass);
+        }
+        return (TO) converter.convert(from);
+    }
 }
